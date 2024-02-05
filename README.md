92ed3c2560a120fbcaed8233ef1c599d79e37c9c
Juraj Piovar <jpiovar@sk.ibm.com>
2024-02-05 15:59:19 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # shared-components
+# react-typescript-package
---
+++ dist/cjs/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MyCounter = void 0;
+var tslib_1 = require("tslib");
+var App_1 = tslib_1.__importDefault(require("./components/App"));
+exports.MyCounter = App_1.default;
+//# sourceMappingURL=index.js.map
