621c631bffabf46dc7cafe8cea42e641f454a656
Juraj Piovar <jpiovar@sk.ibm.com>
2024-02-09 13:11:59 +0100

    update

--- dist/cjs/components/customButton/CustomButton.js
+++ dist/cjs/components/customButton/CustomButton.js
@@ -4,7 +4,7 @@ var tslib_1 = require("tslib");
 var react_1 = tslib_1.__importDefault(require("react"));
 var CustomButton = function () {
     return (react_1.default.createElement("div", null,
-        react_1.default.createElement("button", null, "Custom button")));
+        react_1.default.createElement("button", null, "Custom button 1")));
 };
 exports.default = CustomButton;
 //# sourceMappingURL=CustomButton.js.map
--- dist/esm/components/customButton/CustomButton.js
+++ dist/esm/components/customButton/CustomButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 var CustomButton = function () {
     return (React.createElement("div", null,
-        React.createElement("button", null, "Custom button")));
+        React.createElement("button", null, "Custom button 1")));
 };
 export default CustomButton;
 //# sourceMappingURL=CustomButton.js.map
